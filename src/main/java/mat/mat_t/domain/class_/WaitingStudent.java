85c0c377d3e06e4f034bb1649ba723b5b6f09a74
lsh2613 <lsh2613@naver.com>
2022-08-31 00:37:39 +0900

    wsDto, 중복신청

--- src/main/java/mat/mat_t/domain/class_/WaitingStudent.java
+++ src/main/java/mat/mat_t/domain/class_/WaitingStudent.java
@@ -1,11 +1,13 @@
 package mat.mat_t.domain.class_;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import mat.mat_t.domain.user.Instructor;
 import mat.mat_t.domain.user.User;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -18,18 +20,22 @@ public class WaitingStudent {
 
     //pull 했을때 밑에 부분 주석 처리되어 있었는데 일단 다른거 만들려면 있어야 해서 난 주석 풀고 돌림,,
 //
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id")
     private User userWS;
 //
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "class_id")
     private Classes classesWS;
 //
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "instructor_id")
     private Instructor instructorWS;
 
     private String content;
 
+    private String date;
 }
--- src/main/java/mat/mat_t/web/controller/WaitingStudentController.java
+++ src/main/java/mat/mat_t/web/controller/WaitingStudentController.java
@@ -1,6 +1,7 @@
 package mat.mat_t.web.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import mat.mat_t.domain.class_.Classes;
 import mat.mat_t.domain.class_.WaitingStudent;
@@ -15,8 +16,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,6 +42,7 @@ public class WaitingStudentController {
         waitingStudent.setUserWS(loginUser);
         waitingStudent.setClassesWS(classService.findById(classId));
         waitingStudent.setContent(content);
+        waitingStudent.setDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
         waitingStudentsService.add(waitingStudent);
 
         return ResponseEntity.ok(waitingStudent);
@@ -47,6 +52,28 @@ public class WaitingStudentController {
     @GetMapping("/waitingStudent/list/{classId}")
     public ResponseEntity listStudents(@PathVariable Long classId) {
         List<WaitingStudent> classStudents = waitingStudentsService.findStudentsByClassId(classId);
-        return ResponseEntity.ok(classStudents);
+
+        List<WsDto> classStudentsDto=classStudents.stream()
+                .map(c -> new WsDto(c))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(classStudentsDto);
     }
+
+    @Getter
+    static class WsDto {
+        Long id;
+        String name;
+        String content;
+        String date;
+
+        public WsDto(WaitingStudent waitingStudent) {
+            id = waitingStudent.getWaitingId();
+            name = waitingStudent.getUserWS().getName();
+            content = waitingStudent.getContent();
+            date = waitingStudent.getDate();
+        }
+    }
+
+
 }
