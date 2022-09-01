6852a5b1256fb198342476d9bcff4959ed078436
Ga jeong <rlarkwjd127@gmail.com>
2022-09-02 00:09:50 +0900

    intructor review 수정

--- src/main/java/mat/mat_t/domain/review/InstructorReview.java
+++ src/main/java/mat/mat_t/domain/review/InstructorReview.java
@@ -1,9 +1,9 @@
 package mat.mat_t.domain.review;
 
-
-    import lombok.Getter;
-    import lombok.Setter;
-    import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import mat.mat_t.domain.class_.ClassStatus;
+import mat.mat_t.domain.class_.ClassStudents;
 
 import javax.persistence.*;
 
@@ -16,25 +16,28 @@ public class InstructorReview {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "insRe_id")
     private Long insReviewId;
+
+    private long score;
     private String reviewContent;
-    private float score;
-    
-        public InstructorReview() {
-        }
 
-        public InstructorReview(String reviewContent, float score) {
-            this.score = score;
-            this.reviewContent = reviewContent;
-        }
+    public InstructorReview(String reviewContent, long score) {
+        this.score = score;
+        this.reviewContent = reviewContent;
+    }
 
-        public void update(InstructorReview newInstructorReview) {
-            this.score = newInstructorReview.getScore();
-            this.reviewContent = newInstructorReview.getReviewContent();
-        }
+    public InstructorReview() {
 
-        public InstructorReview getId(Long id){
-            this.insReviewId=id;
-            return null;
-        }
+    }
+
+    public InstructorReview(Long id) {
+        this.insReviewId=id;
+    }
+
+
+    @Override
+    public String toString() {
+        return "InstructorReview{" +
+                "insReviewId=" + insReviewId +
+                '}';
     }
 }
