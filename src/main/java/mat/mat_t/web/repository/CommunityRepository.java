64ac196ec572c91579a197a6f6ffee1d31f5c659
gajeong <rlarkwjd127@gmail.com>
2022-09-21 10:46:33 +0900

    ?

--- src/main/java/mat/mat_t/web/repository/CommunityRepository.java
+++ src/main/java/mat/mat_t/web/repository/CommunityRepository.java
@@ -3,6 +3,6 @@ package mat.mat_t.web.repository;
 import mat.mat_t.domain.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommunityRepository extends JpaRepository<Community, Long> {
+// public interface CommunityRepository extends JpaRepository<Community, Long> {
 
-}
+// }
