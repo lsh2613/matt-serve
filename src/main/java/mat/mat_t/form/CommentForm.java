a24ff2baca5a2dc59e1f30a81c5b58b9ddff2b44
gajeong <rlarkwjd127@gmail.com>
2022-10-11 10:08:36 +0900

    userId로 변경

--- src/main/java/mat/mat_t/form/CommentForm.java
+++ src/main/java/mat/mat_t/form/CommentForm.java
@@ -3,23 +3,22 @@ package mat.mat_t.form;
 import lombok.Data;
 import mat.mat_t.domain.Comment;
 
-
 @Data
 public class CommentForm {
 
     private Long commentId;
     private String content;
-    private String Writer;  //댓글 작성자 닉네임
+    private String Writer; // 댓글 작성자 닉네임
     private Long communityId;
-    private Long instructorId;  //댓글 작성자 고유 id
+    private Long userId; // 댓글 작성자 고유 id
 
-    /* Entity -> Dto*/
+    /* Entity -> Dto */
     public CommentForm(Comment comment) {
         this.commentId = comment.getCommentId();
         this.content = comment.getContent();
         this.Writer = comment.getUserComment().getNickname();
         this.communityId = comment.getCommunity().getId();
-        this.instructorId= comment.getUserComment().getId();
+        this.userId = comment.getUserComment().getId();
     }
 
     public CommentForm() {
